5e6b9e8810f06288c8fa698d968edb807068be60
James White <dev.jameswhite@gmail.com>
2014-02-10 03:23:27 +0000

    Add RSpec and some basic tests

--- Rakefile
+++ Rakefile
@@ -1,5 +1,8 @@
 require 'bundler/gem_tasks'
+require 'rspec/core/rake_task'
+
+RSpec::Core::RakeTask.new 'spec'
 
 task :install_dev do
-  puts `gem uninstall -x minicron && gem build minicron.gemspec && gem install --local ./minicron-*.gem && rm ./minicron-*.gem`
+  puts `gem uninstall -x minicron && gem build minicron.gemspec && gem install --development --local ./minicron-*.gem && rm ./minicron-*.gem`
 end
---
+++ spec/cli_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Minicron::CLI do
+  it 'should run a simple command and print the output to stdout' do
+    output = capture_stdout do
+      Minicron::CLI.new(['run', 'echo hello']).run
+    end
+
+    output.clean == 'hello'
+  end
+
+  it 'should run a simple multi-line command and print the output to stdout' do
+    output = capture_stdout do
+      Minicron::CLI.new(['run', 'ls -l']).run
+    end
+
+    output.clean == `ls -l`.clean
+  end
+
+  it 'should return an error when a non-existent command is run' do
+    capture_stderr do
+      expect {
+        Minicron::CLI.new(['lol']).run
+      }.to raise_error(SystemExit)
+    end
+  end
+end
