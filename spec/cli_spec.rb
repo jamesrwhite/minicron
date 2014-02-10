e021e95274a304b6464290dcfa7b960e617a3e2e
James White <dev.jameswhite@gmail.com>
2014-02-10 23:03:19 +0000

    yield all the thingz!

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,19 +2,15 @@ require 'spec_helper'
 
 describe Minicron::CLI do
   it 'should run a simple command and print the output to stdout' do
-    output = capture_stdout do
-      Minicron::CLI.new(['run', 'echo hello', '--trace']).run
+    Minicron::CLI.new(['run', 'echo hello', '--trace']).run do |output|
+      output.clean == 'hello'
     end
-
-    output.clean == 'hello'
   end
 
   it 'should run a simple multi-line command and print the output to stdout' do
-    output = capture_stdout do
-      Minicron::CLI.new(['run', 'ls -l', '--trace']).run
+    Minicron::CLI.new(['run', 'ls -l', '--trace']).run do |output|
+      output.clean == `ls -l`.clean
     end
-
-    output.clean == `ls -l`.clean
   end
 
   it 'should return an error when a non-existent command is run' do
