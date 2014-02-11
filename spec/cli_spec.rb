cd8eb08e1b3639de115597619f143666b4060cf7
James White <dev.jameswhite@gmail.com>
2014-02-11 22:20:07 +0000

    Restructuring to make run method smaller

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe Minicron::CLI do
   it 'should run a simple command and print the output to stdout' do
-    Minicron::CLI.new.run argv: ['run', 'echo hello', '--trace'], trace: true do |output|
+    Minicron::CLI.new.run ['run', 'echo hello', '--trace'], trace: true do |output|
       output.clean == 'hello'
     end
   end
 
   it 'should run a simple multi-line command and print the output to stdout' do
-    Minicron::CLI.new.run argv: ['run', 'ls -l', '--trace'], trace: true do |output|
+    Minicron::CLI.new.run ['run', 'ls -l', '--trace'], trace: true do |output|
       output.clean == `ls -l`.clean
     end
   end
@@ -16,7 +16,7 @@ describe Minicron::CLI do
   it 'should return an error when a non-existent command is run' do
     capture_stderr do
       expect {
-        Minicron::CLI.new.run argv: ['lol'], trace: true
+        Minicron::CLI.new.run ['lol'], trace: true
       }.to raise_error(SystemExit)
     end
   end
@@ -24,7 +24,7 @@ describe Minicron::CLI do
   it 'should return an error when tracing is disabled but passed as an option' do
     capture_stderr do
       expect {
-        Minicron::CLI.new.run argv: ['run', 'echo 1', '--trace']
+        Minicron::CLI.new.run ['run', 'echo 1', '--trace']
       }.to raise_error(SystemExit)
     end
   end
