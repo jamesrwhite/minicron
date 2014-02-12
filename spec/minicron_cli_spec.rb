fe26a74baea7435641e57fc0501fa7275da4e0f2
James White <dev.jameswhite@gmail.com>
2014-02-12 03:47:32 +0000

    - Moved capture_stdout and capture_stderr commands to Minicron module and merged them to make better use of them - Standardised on hash syntax used - Use new Minicron.capture_output to catch commander stderr output and print it back out again - Minicron.capture_output tests - Restructured Minicron::CLI tests

---
+++ spec/minicron_cli_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Minicron::CLI do
+  describe '#run' do
+    it 'should run a simple command and print the output to stdout' do
+      Minicron::CLI.new.run ['run', 'echo hello', '--trace'], :trace => true do |output|
+        output.clean == 'hello'
+      end
+    end
+
+    it 'should run a simple multi-line command and print the output to stdout' do
+      Minicron::CLI.new.run ['run', 'ls -l', '--trace'], :trace => true do |output|
+        output.clean == `ls -l`.clean
+      end
+    end
+
+    it 'should return an error when a non-existent command is run' do
+      Minicron.capture_output :type => :stderr do
+        expect {
+          Minicron::CLI.new.run ['lol'], :trace => true
+        }.to raise_error(SystemExit)
+      end
+    end
+
+    it 'should return an error when tracing is disabled but passed as an option' do
+      Minicron.capture_output :type => :stderr do
+        expect {
+          Minicron::CLI.new.run ['run', 'echo 1', '--trace']
+        }.to raise_error(SystemExit)
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,38 +2,11 @@
 # Coveralls.wear!
 
 require 'rspec'
-require 'minicron/cli'
-require 'stringio'
+require 'minicron'
 
 RSpec.configure do |config|
   config.color_enabled = true
   config.formatter     = 'documentation'
-end
-
-# Capture stdout by mocking it and then reset it
-# adapted from http://stackoverflow.com/a/11349621/483271
-def capture_stdout(&block)
-  original_stdout = $stdout
-  $stdout = fake = StringIO.new
-  begin
-    yield
-  ensure
-    $stdout = original_stdout
-  end
-  fake.string
-end
-
-# Capture stderr by mocking it and then reset it
-# adapted from http://stackoverflow.com/a/11349621/483271
-def capture_stderr(&block)
-  original_stderr = $stderr
-  $stderr = fake = StringIO.new
-  begin
-    yield
-  ensure
-    $stderr = original_stderr
-  end
-  fake.string
 end
 
 # Normalise varied new line usage
