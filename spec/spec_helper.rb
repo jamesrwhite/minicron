0d6df3aef0da5beb82c58cfb0634da65299396b6
James White <dev.jameswhite@gmail.com>
2014-02-12 20:51:21 +0000

    That was a bad idea, coverall was using my custom SimpleCov set up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
 if ENV['CI'] && (!defined?(RUBY_ENGINE) || RUBY_ENGINE == 'ruby')
   require 'coveralls'
   Coveralls.wear!
-else
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter '/spec/'
-  end
+end
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
 end
 
 require 'rspec'
