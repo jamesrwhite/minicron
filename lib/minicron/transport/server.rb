4abf05011fb8d863d354e535f08737645cd7cc8d
James White <dev.jameswhite@gmail.com>
2014-02-19 22:32:48 +0000

    Documentation, yo.

--- lib/minicron/transport/server.rb
+++ lib/minicron/transport/server.rb
@@ -7,6 +7,11 @@ module Minicron
     class Server
       attr_accessor :server
 
+      # Starts the thin server
+      #
+      # @param host [String] the host e.g 0.0.0.0
+      # @param port [Integer]
+      # @param path [String] The absolute path to the server e.g /server
       def start!(host, port, path)
         return false if running?
 
@@ -20,22 +25,27 @@ module Minicron
         )
 
         faye.on(:handshake) do |client_id|
+          # TODO: Respect the --verbose option here
           p [:handshake, client_id]
         end
 
         faye.on(:subscribe) do |client_id, channel|
+          # TODO: Respect the --verbose option here
           p [:subscribe, client_id, channel]
         end
 
         faye.on(:unsubscribe) do |client_id, channel|
+          # TODO: Respect the --verbose option here
           p [:unsubscribe, client_id, channel]
         end
 
         faye.on(:publish) do |client_id, channel, data|
+          # TODO: Respect the --verbose option here
           p [:published, client_id, channel, data]
         end
 
         faye.on(:disconnect) do |client_id|
+          # TODO: Respect the --verbose option here
           p [:disconnect, client_id]
         end
 
@@ -53,6 +63,8 @@ module Minicron
         true
       end
 
+      # Stops the thin server if it's running
+      # @return [Boolean] whether the server was stopped or not
       def stop!
         return false unless running? && server != nil
 
@@ -60,6 +72,8 @@ module Minicron
         true
       end
 
+      # Returns a bool based on whether
+      # @return [Boolean]
       def running?
         return false unless server != nil
 
