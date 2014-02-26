91b9f783e9ac2fb250653a13dc464cfb1aa9ce40
James White <dev.jameswhite@gmail.com>
2014-02-26 00:48:01 +0000

    Default the name of the command to the command if the name isn't set

--- lib/minicron/hub/models/job.rb
+++ lib/minicron/hub/models/job.rb
@@ -1,4 +1,13 @@
 class Job < ActiveRecord::Base
   has_many :executions
   has_many :job_execution_outputs
+
+  # Default the name of the command to the command itself if no name is set
+  def name
+    if read_attribute(:name) == ''
+      read_attribute(:command)
+    else
+      read_attribute(:name)
+    end
+  end
 end
