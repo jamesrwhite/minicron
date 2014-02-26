b557d6f6807090dab3ba85a3ddc9e9a9f70b25ed
James White <dev.jameswhite@gmail.com>
2014-02-26 01:35:54 +0000

    Added API methods for executions and showing them in the frontend

--- lib/minicron/hub/controllers/api.rb
+++ lib/minicron/hub/controllers/api.rb
@@ -1,4 +1,5 @@
 require 'minicron/hub/models/job'
+require 'minicron/hub/models/execution'
 
 class Minicron::Hub::App
   get '/api/jobs.json' do
@@ -9,5 +10,18 @@ class Minicron::Hub::App
   get '/api/jobs/:job_id.json' do
     content_type :json
     Job.find(params[:job_id]).to_json
+  end
+
+  get '/api/executions.json' do
+    content_type :json
+    Execution.all.order(:started_at => :desc).includes(:job)
+             .to_json(:include => :job)
+  end
+
+  get '/api/executions/:execution_id.json' do
+    content_type :json
+    Execution.includes(:job, :job_execution_output)
+             .find(params[:execution_id])
+             .to_json(:include => [:job, :job_execution_output])
   end
 end
