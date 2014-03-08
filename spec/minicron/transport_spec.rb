26b18c5e7f6087542df22cbd4450d389c7e5a68a
James White <dev.jameswhite@gmail.com>
2014-03-08 17:45:16 +0000

    Fix test

--- spec/minicron/transport_spec.rb
+++ spec/minicron/transport_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe Minicron::Transport do
   describe '.get_job_id' do
     context 'when the correct params are passed' do
-      it 'should return a 40 char string (sha1 hash)' do
+      it 'should return a 32 char string (md5 hash)' do
         hash = Minicron::Transport.get_job_id('ls', 'server1')
 
-        expect(hash.length).to eq 40
+        expect(hash.length).to eq 32
       end
     end
   end
