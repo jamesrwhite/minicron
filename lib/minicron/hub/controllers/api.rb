acb26ed88318c0e7d6e7c4bad470d6fd916adecc
James White <dev.jameswhite@gmail.com>
2014-03-12 22:48:27 +0000

    Order hosts by id

--- lib/minicron/hub/controllers/api.rb
+++ lib/minicron/hub/controllers/api.rb
@@ -3,15 +3,14 @@ class Minicron::Hub::App
   # TODO: Add offset/limit
   get '/api/hosts' do
     content_type :json
-    hosts = Minicron::Hub::Host.all.includes(:jobs).order(:created_at => :desc)
+    hosts = Minicron::Hub::Host.all.includes(:jobs).order(:id => :asc)
     HostSerializer.new(hosts).serialize.to_json
   end
 
   # Get a single host by its ID
   get '/api/hosts/:id' do
     content_type :json
-    host = Minicron::Hub::Host.includes(:jobs).order(:created_at => :desc)
-                              .find(params[:id])
+    host = Minicron::Hub::Host.includes(:jobs).find(params[:id])
     HostSerializer.new(host).serialize.to_json
   end
 
