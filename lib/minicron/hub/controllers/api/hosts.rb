64800f94c6dc75990acc2d890ca4b0380a252e64
James White <dev.jameswhite@gmail.com>
2014-03-13 01:51:03 +0000

    Basic host creation

--- lib/minicron/hub/controllers/api/hosts.rb
+++ lib/minicron/hub/controllers/api/hosts.rb
@@ -13,4 +13,23 @@ class Minicron::Hub::App
     host = Minicron::Hub::Host.includes(:jobs).find(params[:id])
     HostSerializer.new(host).serialize.to_json
   end
+
+  post '/api/hosts' do
+    begin
+      # Load the JSON body
+      request_body = Oj.load(request.body)
+
+      # Try and save the new host
+      host = Minicron::Hub::Host.create(
+        :name => request_body['host']['name'],
+        :hostname => request_body['host']['hostname']
+      )
+
+      # Return the new host
+      HostSerializer.new(host).serialize.to_json
+    # TODO: nicer error handling here with proper validation before hand
+    rescue Exception => e
+      { :error => e.message }.to_json
+    end
+  end
 end
