8b5937590e4524ad63e38729fac41ac216e11ca8
James White <dev.jameswhite@gmail.com>
2015-04-18 11:28:20 +0100

    Basic sidebar loading of recent executions

--- lib/minicron/hub/controllers/index.rb
+++ lib/minicron/hub/controllers/index.rb
@@ -1,5 +1,10 @@
 class Minicron::Hub::App
   get '/' do
+    # Get a list of recent executions for the sidebar
+    @recent = Minicron::Hub::Execution.all
+                                      .order(:created_at => :desc, :started_at => :desc)
+                                      .includes(:job => :host)
+
     erb :index, :layout => :'layouts/app'
   end
 end
