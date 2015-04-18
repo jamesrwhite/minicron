2e0b24e525bd0e6f1976efa6f920673a4ffbe67a
James White <dev.jameswhite@gmail.com>
2015-04-18 11:47:07 +0100

    Remove lots of un-needed assets

--- lib/minicron/hub/app.rb
+++ lib/minicron/hub/app.rb
@@ -57,8 +57,7 @@ module Minicron::Hub
         '/js/moment-2.5.1.min.js',
         '/js/perfect-scrollbar-0.4.10.with-mousewheel.min.js',
 
-        # Ember application files
-        '/app/**/*.js'
+        '/js/application.js'
       ]
     end
 
