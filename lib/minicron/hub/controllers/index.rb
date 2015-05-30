0322cec3998fdb6b0011610c2910f731657f4dad
James White <dev.jameswhite@gmail.com>
2015-05-30 10:54:19 +0100

    View alerts

--- lib/minicron/hub/controllers/index.rb
+++ lib/minicron/hub/controllers/index.rb
@@ -1,9 +1,4 @@
 class Minicron::Hub::App
-  # Get a list of recent executions for the sidebar
-  @recent_executions = Minicron::Hub::Execution.all
-                                               .order(:created_at => :desc, :started_at => :desc)
-                                               .includes(:job => :host)
-
   get '/' do
     # Get the most recent job execution
     recent = Minicron::Hub::Execution
--- lib/minicron/hub/models/alert.rb
+++ lib/minicron/hub/models/alert.rb
@@ -3,6 +3,7 @@ autoload :ActiveRecord, 'sinatra/activerecord'
 module Minicron
   module Hub
     class Alert < ActiveRecord::Base
+      belongs_to :job
       belongs_to :schedule
       belongs_to :execution
     end
