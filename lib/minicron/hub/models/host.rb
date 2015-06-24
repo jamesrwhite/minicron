f7a6c96ea0c6105bc932f407e4f26e38ab200f8c
James White <james.white@miniclip.com>
2015-06-24 14:04:07 +0100

    Basic host validation

--- lib/minicron/hub/models/host.rb
+++ lib/minicron/hub/models/host.rb
@@ -5,6 +5,12 @@ module Minicron
     class Host < ActiveRecord::Base
       has_many :jobs, :dependent => :destroy
 
+      validates :name, :presence => true, :uniqueness => true
+      validates :fqdn, :presence => true
+      validates :user, :presence => true
+      validates :host, :presence => true
+      validates :port, :presence => true, :numericality => { :only_integer => true }
+
       # Default the name of the host to the fqdn itself if no name is set
       def name
         if read_attribute(:name) == '' || read_attribute(:name).nil?
