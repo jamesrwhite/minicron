be86f760a8c8e50402c6fc547c11c6c8b3a92ac5
James White <dev.jameswhite@gmail.com>
2016-01-02 12:58:09 +0000

    Refactored to build the full crontab manually

--- lib/minicron/cron.rb
+++ lib/minicron/cron.rb
@@ -1,5 +1,6 @@
 require 'shellwords'
 require 'escape'
+require 'securerandom'
 
 module Minicron
   # Used to interact with the crontab on hosts over an ssh connection
@@ -11,242 +12,113 @@ module Minicron
       @ssh = ssh
     end
 
-    # Build the minicron command to be used in the crontab
-    #
-    # @param schedule [String]
-    # @param user [String]
-    # @param command [String]
-    # @return [String]
-    def build_minicron_command(schedule, user, command)
-      # Escape the command so it will work in bourne shells
-      command = Escape.shell_command(['minicron', 'run', command])
-      cron_command = Escape.shell_command(['/bin/bash', '-l', '-c', command])
-
-      "#{schedule} #{user} #{cron_command}"
-    end
-
     # Test an SSH connection and the permissions for the crontab
     #
     # @param conn an instance of an open ssh connection
     # @return [Hash]
-    def test_host_permissions(conn = nil)
+    def get_host_permissions(conn = nil)
       begin
         # Open an SSH connection
         conn ||= @ssh.open
-
-        # Check if /etc is writeable
-        etc_write = conn.exec!("/bin/sh -c 'test -w /etc && echo \"y\" || echo \"n\"'").strip
-
-        # Check if /etc is executable
-        etc_execute = conn.exec!("/bin/sh -c 'test -x /etc && echo \"y\" || echo \"n\"'").strip
-
-        # Check if the crontab is readable
-        crontab_read = conn.exec!("/bin/sh -c 'test -r #{Minicron.config['server']['cron_file']} && echo \"y\" || echo \"n\"'").strip
-
-        # Check if the crontab is writeable
-        crontab_write = conn.exec!("/bin/sh -c 'test -w #{Minicron.config['server']['cron_file']} && echo \"y\" || echo \"n\"'").strip
       rescue
-        conn = etc_write = etc_execute = crontab_read = crontab_write = false
+        conn = false
       end
 
       {
         :connect => conn != false,
-        :etc => {
-          :write => etc_write == 'y',
-          :execute => etc_execute == 'y'
-        },
-        :crontab => {
-          :read => crontab_read == 'y',
-          :write => crontab_write == 'y'
-        }
       }
     end
 
-    # Used to find a string and replace it with another in the crontab by
-    # using the sed command
+    # Test if a host has correct permissions for minicron to operate
     #
     # @param conn an instance of an open ssh connection
-    # @param find [String]
-    # @param replace [String]
-    def find_and_replace(conn, find, replace)
+    def test_host_permissions(conn)
       begin
         # Test the SSH connection first
-        test = test_host_permissions(conn)
+        test = get_host_permissions(conn)
       rescue Exception => e
         raise Minicron::CronError, "Error connecting to host, reason: #{e.message}"
       end
 
       # Check the connection worked
       raise Minicron::CronError, "Unable to connect to host, reason: unknown" if !test[:connect]
-
-      # Check the crontab is readable
-      raise Minicron::CronError, "Insufficient permissions to read from the cron file" if !test[:crontab][:read]
-
-      # Check the crontab is writeable
-      raise Minicron::CronError, "Insufficient permissions to write to the cron file" if !test[:crontab][:write]
-
-      # Check /etc is writeable
-      raise Minicron::CronError, "/etc is not writeable by the current user" if !test[:etc][:write]
-
-      # Check /etc is executable
-      raise Minicron::CronError, "/etc is not executable by the current user" if !test[:etc][:execute]
-
-      # Get the full crontab
-      crontab = conn.exec!("cat #{Minicron.config['server']['cron_file']}").to_s.strip
-
-      # Replace the full string with the replacement string
-      begin
-        crontab[find] = replace
-      rescue Exception => e
-        raise Minicron::CronError, "Unable to replace '#{find}' with '#{replace}' in the cron file, reason: #{e}"
-      end
-
-      # Echo the crontab back to the tmp crontab
-      conn.exec!("echo #{crontab.shellescape} > /tmp/minicron_crontab").to_s.strip
-
-      # If it's a delete
-      if replace == ''
-        # Check the original line is no longer there
-        grep = conn.exec!("grep -F #{find.shellescape} /tmp/minicron_crontab").to_s.strip
-
-        # Throw an exception if we can't see our new line at the end of the file
-        if grep != replace
-          raise Minicron::CronError, "Expected to find nothing when grepping the cron file but found #{grep}"
-        end
-      else
-        # Check the updated line is there
-        grep = conn.exec!("grep -F #{replace.shellescape} /tmp/minicron_crontab").to_s.strip
-
-        # Throw an exception if we can't see our new line at the end of the file
-        if grep != replace
-          raise Minicron::CronError, "Expected to find '#{replace}' when grepping the cron file but found #{grep}"
-        end
-      end
-
-      # And finally replace the crontab with the new one now we now the change worked
-      move = conn.exec!("/bin/sh -c 'mv /tmp/minicron_crontab #{Minicron.config['server']['cron_file']} && echo \"y\" || echo \"n\"'").to_s.strip
-
-      if move != 'y'
-        raise Minicron::CronError, "Unable to move /tmp/minicron_crontab to #{Minicron.config['server']['cron_file']}, check the permissions?"
-      end
     end
 
-    # Add the schedule for this job to the crontab
+    # Build the minicron command to be used in the crontab
     #
-    # @param job [Minicron::Hub::Job] an instance of a job model
-    # @param schedule [String] the job schedule as a string
-    # @param conn an instance of an open ssh connection
-    def add_schedule(job, schedule, conn = nil)
-      # Open an SSH connection
-      conn ||= @ssh.open
-
-      # Prepare the line we are going to write to the crontab
-      line = build_minicron_command(schedule, job.user, job.command)
-      escaped_line = line.shellescape
-      echo_line = "echo #{escaped_line} >> #{Minicron.config['server']['cron_file']}"
-
-      # Append it to the end of the crontab
-      conn.exec!(echo_line).to_s.strip
-
-      # Check the line is there
-      tail = conn.exec!("tail -n 1 #{Minicron.config['server']['cron_file']}").to_s.strip
-
-      # Throw an exception if we can't see our new line at the end of the file
-      if tail != line
-        raise Minicron::CronError, "Expected to find '#{line}' at EOF but found '#{tail}'"
-      end
+    # @param schedule [String]
+    # @param command [String]
+    # @return [String]
+    def build_minicron_command(schedule, command)
+      # Escape the command so it will work in bourne shells
+      "#{schedule} #{Escape.shell_command(['minicron', 'run', command])}"
     end
 
-    # Update the schedule for this job in the crontab
+    # Build the crontab multiline string that includes all the given jobs
     #
-    # @param job [Minicron::Hub::Job] an instance of a job model
-    # @param old_schedule [String] the old job schedule as a string
-    # @param new_schedule [String] the new job schedule as a string
-    # @param conn an instance of an open ssh connection
-    def update_schedule(job, old_schedule, new_schedule, conn = nil)
-      # Open an SSH connection
-      conn ||= @ssh.open
+    # @param host [Minicron::Hub::Host] a host instance with it's jobs and job schedules
+    # @return [String]
+    def build_crontab(host)
+      # You have been warned..
+      crontab = "#\n"
+      crontab += "# This file was automatically generated by minicron at #{Time.now.to_s}, DO NOT EDIT manually!\n"
+      crontab += "#\n\n"
 
-      # We are looking for the current value of the schedule
-      find = build_minicron_command(old_schedule, job.user, job.command)
+      # Set the path to something sensible by default, eventually this should be configurable
+      crontab += "# ENV variables\n"
+      crontab += "PATH=/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin\n\n"
 
-      # And replacing it with the updated value
-      replace = build_minicron_command(new_schedule, job.user, job.command)
-
-      # Replace the old schedule with the new schedule
-      find_and_replace(conn, find, replace)
-    end
-
-    # Update the user/command for a job and all its schedules
-    #
-    # @param old_job [Minicron::Hub::Job] the old job
-    # @param new_job [Minicron::Hub::Job] the new job
-    # @param conn an instance of an open ssh connection
-    def update_job(old_job, new_job, conn = nil)
-      conn ||= @ssh.open
-
-      # Loop through each schedule and delete them one by one
-      # TODO: what if one schedule update fails but others don't? Should
-      # we try and rollback somehow or just return the job with half its
-      # schedules deleted?
-      new_job.schedules.each do |schedule|
-        # We are looking for the current value of the schedule
-        find = build_minicron_command(schedule.formatted, old_job.user, old_job.command)
-
-        # And replacing it with the updated value
-        replace = build_minicron_command(schedule.formatted, new_job.user, new_job.command)
-
-        # Replace the old schedule with the new schedule
-        find_and_replace(conn, find, replace)
-      end
-    end
-
-    # Remove the schedule for this job from the crontab
-    #
-    # @param job [Minicron::Hub::Job] an instance of a job model
-    # @param schedule [String] the job schedule as a string
-    # @param conn an instance of an open ssh connection
-    def delete_schedule(job, schedule, conn = nil)
-      # Open an SSH connection
-      conn ||= @ssh.open
-
-      # We are looking for the current value of the schedule
-      find = build_minicron_command(schedule, job.user, job.command)
-
-      # Replace the old schedule with nothing i.e deleting it
-      find_and_replace(conn, find, '')
-    end
-
-    # Delete a job and all it's schedules from the crontab
-    #
-    # @param job [Minicron::Hub::Job] a job instance with it's schedules
-    # @param conn an instance of an open ssh connection
-    def delete_job(job, conn = nil)
-      conn ||= @ssh.open
-
-      # Loop through each schedule and delete them one by one
-      # TODO: what if one schedule removal fails but others don't? Should
-      # we try and rollback somehow or just return the job with half its
-      # schedules deleted?
-      job.schedules.each do |schedule|
-        delete_schedule(job, schedule.formatted, conn)
-      end
-    end
-
-    # Delete a host and all it's jobs from the crontab
-    #
-    # @param job [Minicron::Hub::Job] a job instance with it's schedules
-    # @param conn an instance of an open ssh connection
-    def delete_host(host, conn = nil)
-      conn ||= @ssh.open
-
-      # Loop through each job and delete them one by one
-      # TODO: what if one job removal fails but others don't? Should
-      # we try and rollback somehow or just return the host with half its
-      # jobs deleted?
+      # Add an entry to the crontab for each job schedule
       host.jobs.each do |job|
-        delete_job(job, conn)
+        crontab += "# Job ##{job.id} - #{job.name}\n"
+
+        job.schedules.each do |schedule|
+          crontab += "#{build_minicron_command(schedule.formatted, job.command)}\n"
+        end
       end
+
+      crontab
+    end
+
+    # Update the crontab on the given host
+    #
+    # @param host [Minicron::Hub::Host] a host instance with it's jobs and job schedules
+    # @param conn an instance of an open ssh connection
+    def update_crontab(host, conn = nil)
+      # Open an SSH connection
+      conn ||= @ssh.open
+
+      # First check if we have the correct permissions we need
+      test_host_permissions(conn)
+
+      # Build the new crontab to set
+      crontab = build_crontab(host).strip
+
+      # Generate a temporary name for the temp crontab
+      tmp_crontab_name = "/tmp/minicron_tmp_crontab_#{SecureRandom.uuid}"
+
+      # Echo the crontab to the tmp crontab file
+      conn.exec!("echo #{crontab.shellescape} > #{tmp_crontab_name}").to_s.strip
+
+      # Check that the file has the contents we expect
+      tmp_crontab = conn.exec!("cat #{tmp_crontab_name}").to_s.strip
+
+      if tmp_crontab != crontab
+        raise Minicron::CronError, "The contents of #{tmp_crontab_name} were not as expected, maybe the file couldn't be written to?"
+      end
+
+      # Install the crontab
+      conn.exec!("crontab #{tmp_crontab_name}").to_s.strip
+
+      # Check the updated crontab is set to what we expect
+      updated_crontab = conn.exec!("crontab -l").to_s.strip
+
+      if updated_crontab != crontab
+        raise Minicron::CronError, 'The contents of the updated were not as expected'
+      end
+
+      # ..and finally, remove the temp crontab
+      conn.exec!("rm #{tmp_crontab_name}").to_s.strip
     end
   end
 end
