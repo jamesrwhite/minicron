234c292783b0088246647ce0872947a2dd6ed85b
James White <dev.jameswhite@gmail.com>
2016-04-11 23:23:55 +0100

    Bump version

--- lib/minicron/constants.rb
+++ lib/minicron/constants.rb
@@ -1,7 +1,7 @@
 # The minicron module
 module Minicron
   BINARY_NAME = 'minicron'
-  VERSION = '0.9.3'
+  VERSION = '0.9.4'
   DEFAULT_CONFIG_FILE = '/etc/minicron.toml'
   BASE_PATH = File.expand_path('../../../', __FILE__)
   DB_PATH = "#{BASE_PATH}/db"
