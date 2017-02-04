b26c4ff01245261a7f85deac1fc1d51fd6b4d775
James White <james.white@songkick.com>
2017-02-04 13:25:52 +0000

    Optimise execution query to sort in code

--- lib/minicron/hub/controllers/executions.rb
+++ lib/minicron/hub/controllers/executions.rb
@@ -1,9 +1,8 @@
 class Minicron::Hub::App
   get '/execution/:id' do
     # Look up the job execution
-    @execution = Minicron::Hub::Execution.includes(:job_execution_outputs, job: :host)
-                                         .order('job_execution_outputs.seq')
-                                         .find(params[:id])
+    @execution = Minicron::Hub::Execution.includes(:job_execution_outputs, job: :host).find(params[:id])
+    @execution.job_execution_outputs.sort { |a,b| a.seq <=> b.seq }
 
     erb :'executions/show', layout: :'layouts/app'
   end
