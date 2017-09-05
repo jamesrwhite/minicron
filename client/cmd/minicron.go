abb6c29e48f7c6fb1e91455ba336c545274dca36
James White <james.white@minicron.com>
2017-09-05 21:01:44 +0100

    Remove pointless comments

--- client/cmd/minicron.go
+++ client/cmd/minicron.go
@@ -9,13 +9,10 @@ import (
 
 var Verbose bool
 
-// RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use: "minicron",
 }
 
-// Execute adds all child commands to the root command sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
